8a608766329f5ea365af9d4386887ef09db03261
Felipe <felipefreitasdaschagas@hotmail.com>
2022-04-20 06:27:59 -0300

    carregando os dados do local storage para aparecerem na front da aplicação

--- componentes/criaTarefa.js
+++ componentes/criaTarefa.js
@@ -6,7 +6,6 @@ export const handleNovoItem = (evento) => {
     evento.preventDefault()
 
     const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
-    const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
 
@@ -20,10 +19,6 @@ export const handleNovoItem = (evento) => {
     }
 
     const tarefasAtualizadas = [...tarefas, dados]
-
-    const criaTarefa = Tarefa(dados)
-
-    lista.appendChild(criaTarefa)
 
     localStorage.setItem("tarefas", JSON.stringify(tarefasAtualizadas))
 
